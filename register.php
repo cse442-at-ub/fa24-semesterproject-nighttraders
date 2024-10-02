37956cdbea8b8930de481965d9bc1e550b67657a
Dominic <domlincog@gmail.com>
2024-10-01 20:36:37 -0400

    Added instructions on how the database is set up and how to add to it

--- register.php
+++ register.php
@@ -14,8 +14,28 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "If you are reading this than the connection was successful!"
+echo "If you are reading this than the connection was successful!";
 
 
+/* READ THIS FOR INFO ON DATABASE
+//The database takes in these fields:
 
+$username = "testuser"; //up to 100 charaters long max
+$password = "testpassword"; // up to 100 characters long max
+$birthdate = "2000-01-01";
+$email = "testuser@example.com"; //up to 150 characters long max
+
+//And you would insert into the database like this
+$sql = "INSERT INTO users (id, username, password, birthdate, email) VALUES (NULL, '$username', '$password', '$birthdate', '$email')";
+//Make sure you set the id to null because it is auto-incrimenting
+
+if ($conn->query($sql) === TRUE) {
+    echo "Added user to the users table";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+*/
+
+$conn->close();
 ?>
