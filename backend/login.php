e6efb142724bd61bce62cb43f64f4681fd69b648
Ghesar <ghesar4phanday@gmail.com>
2024-10-03 12:44:08 -0400

    added login backend

---
+++ backend/login.php
@@ -0,0 +1,36 @@
+<?php
+
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header("Access-Control-Allow-Methods: POST, GET");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
+
+include_once('db.php');
+
+session_start();
+if (isset($_SESSION["user"])) {
+    // redirect to dashboard
+    // header("Location: .php");
+    }
+
+if (isset($_POST["login"])) {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $sql = "SELECT * FROM users WHERE email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    if ($user) {
+        if (password_verify($password, $user["password"])) {
+            session_start();
+            $_SESSION["user"] = "yes";
+            // redirect to dashboard
+            // header("Location: .php");
+            die();
+        }else{
+            die(json_encode(['error' => 'Invalid credentials.']));
+        }
+    }else{
+        die(json_encode(['error' => 'Invalid credentials.']));
+    }
+}
+?>
--- backend/register.php
+++ backend/register.php
@@ -1,5 +1,10 @@
 <?php
 
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header("Access-Control-Allow-Methods: POST, GET");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
+
 include_once('db.php');
 
 // extract data from input
@@ -11,29 +16,37 @@ $passwordRepeat = $_POST["repeat_password"];
 $passwordHash = password_hash($password, PASSWORD_BCRYPT);
 
 if (empty($username) OR empty($email) OR empty($password) OR empty($passwordRepeat)) {
-    die(json_encode(['error' => 'All fields are required.']));
+    die(json_encode(['error' => 'All fields are required']));
 }
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    die(json_encode(['error' => 'Email is not valid.']));
+    die(json_encode(['error' => 'Email is not valid']));
 }
 if (strlen($password)<8) {
-    die(json_encode(['error' => 'An account with this email already exists.']));
+    die(json_encode(['error' => 'Password must be at least 8 characters']));
 }
 if (!preg_match('/[A-Z]/', $password)){
-    die(json_encode(['error' => 'Password requires at least one capital letter.']));
+    die(json_encode(['error' => 'Password requires at least one capital letter']));
 }
 $specialChars = "!@#$%^&*()_+-=[]{}|;:,.<>?/~`";
 if (!preg_match('/[' . preg_quote($specialChars, '/') . ']/', $password)){
-    die(json_encode(['error' => 'Password requires at least one symbol.']));
+    die(json_encode(['error' => 'Password requires at least one symbol']));
 }
 if ($password!==$passwordRepeat) {
-    die(json_encode(['error' => 'Passwords do not match.']));
+    die(json_encode(['error' => 'Passwords do not match']));
 }
+// check if email is already registered
 $sql = "SELECT * FROM users WHERE email = '$email'";
 $result = mysqli_query($conn, $sql);
 $rowCount = mysqli_num_rows($result);
 if ($rowCount>0) {
-    die(json_encode(['error' => 'The email is already registered.']));
+    die(json_encode(['error' => 'The email is already registered']));
+}
+// check if username is already registered
+$sql = "SELECT * FROM users WHERE usernamne = '$username'";
+$result = mysqli_query($conn, $sql);
+$rowCount = mysqli_num_rows($result);
+if ($rowCount>0) {
+    die(json_encode(['error' => 'The username is already registered']));
 }
 
 $sql = "INSERT INTO users (username, passwordHash, birthdate, email) VALUES (?, ?, ?, ? )";
