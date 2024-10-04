bb8195355719436c2f4bb3a135084f8be63a4055
Dominic <domlincog@gmail.com>
2024-10-03 20:05:51 -0400

    fixed critical bugs (typo in username column name and no column named passwordHash)

--- backend/register.php
+++ backend/register.php
@@ -13,7 +13,7 @@ $password = $_POST["password"];  // up to 100 characters long
 $birthdate = $_POST["birthday"]; //up to 150 characters long 
 $email = $_POST["email"];
 $passwordRepeat = $_POST["repeat_password"];
-$passwordHash = password_hash($password, PASSWORD_BCRYPT);
+$passwordHash = password_hash($password, PASSWORD_BCRYPT); // create hashed password
 
 if (empty($username) OR empty($email) OR empty($password) OR empty($passwordRepeat)) {
     die(json_encode(['error' => 'All fields are required']));
@@ -34,6 +34,7 @@ if (!preg_match('/[' . preg_quote($specialChars, '/') . ']/', $password)){
 if ($password!==$passwordRepeat) {
     die(json_encode(['error' => 'Passwords do not match']));
 }
+
 // check if email is already registered
 $sql = "SELECT * FROM users WHERE email = '$email'";
 $result = mysqli_query($conn, $sql);
@@ -41,18 +42,20 @@ $rowCount = mysqli_num_rows($result);
 if ($rowCount>0) {
     die(json_encode(['error' => 'The email is already registered']));
 }
+
 // check if username is already registered
-$sql = "SELECT * FROM users WHERE usernamne = '$username'";
+$sql = "SELECT * FROM users WHERE username = '$username'";
 $result = mysqli_query($conn, $sql);
 $rowCount = mysqli_num_rows($result);
 if ($rowCount>0) {
     die(json_encode(['error' => 'The username is already registered']));
 }
 
-$sql = "INSERT INTO users (username, passwordHash, birthdate, email) VALUES (?, ?, ?, ? )";
+// Insert the user into the database
+$sql = "INSERT INTO users (username, password, birthdate, email) VALUES (?, ?, ?, ?)";
 $stmt = $conn->prepare($sql);
 if ($stmt) {
-    $stmt->bind_param("ssss", $username, $hashed_password, $birthdate, $email);
+    $stmt->bind_param("ssss", $username, $passwordHash, $birthdate, $email); // pass the hashed password into the "password" column
     $stmt->execute();
 }else{
     die("Something went wrong");
