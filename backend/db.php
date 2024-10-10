2f89bc577861b0019d7d9a1f5b3fdc1138a66954
Ghesar <ghesar4phanday@gmail.com>
2024-10-10 14:39:52 -0400

    added more security to register

--- backend/db.php
+++ backend/db.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "usersTest";
+$dbname = "users";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
--- backend/register.php
+++ backend/register.php
@@ -1,62 +1,73 @@
 <?php
-
+include_once('db.php');
 header('Access-Control-Allow-Origin: http://localhost:3000');
 header("Access-Control-Allow-Methods: POST, GET");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
-include_once('db.php');
+if(
+    (!isset($_SERVER['HTTPS'])||
+    ($_SERVER['HTTPS']!='on')))
+    {
+    header('Location: '.
+    'https://'.
+    $_SERVER['SERVER_NAME'].
+    $_SERVER['PHP_SELF']);
+    }
 
 // extract data from input
-$username = $_POST["username"]; //up to 100 charaters long
-$password = $_POST["password"];  // up to 100 characters long
-$birthdate = $_POST["birthday"]; //up to 150 characters long 
-$email = $_POST["email"];
-$passwordRepeat = $_POST["repeat_password"];
-$passwordHash = password_hash($password, PASSWORD_BCRYPT);
+if (isset($_POST["submit"])) {
+    $username = $_POST["username"]; 
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $birthday = date('Y-m-d', strtotime(trim($_POST["birthday"])));
+    $password = trim($_POST["password"]);
+    $passwordRepeat = trim($_POST["repeat_password"]);
 
-if (empty($username) OR empty($email) OR empty($password) OR empty($passwordRepeat)) {
-    die(json_encode(['error' => 'All fields are required']));
-}
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    die(json_encode(['error' => 'Email is not valid']));
-}
-if (strlen($password)<8) {
-    die(json_encode(['error' => 'Password must be at least 8 characters']));
-}
-if (!preg_match('/[A-Z]/', $password)){
-    die(json_encode(['error' => 'Password requires at least one capital letter']));
-}
-$specialChars = "!@#$%^&*()_+-=[]{}|;:,.<>?/~`";
-if (!preg_match('/[' . preg_quote($specialChars, '/') . ']/', $password)){
-    die(json_encode(['error' => 'Password requires at least one symbol']));
-}
-if ($password!==$passwordRepeat) {
-    die(json_encode(['error' => 'Passwords do not match']));
-}
-// check if email is already registered
-$sql = "SELECT * FROM users WHERE email = '$email'";
-$result = mysqli_query($conn, $sql);
-$rowCount = mysqli_num_rows($result);
-if ($rowCount>0) {
-    die(json_encode(['error' => 'The email is already registered']));
-}
-// check if username is already registered
-$sql = "SELECT * FROM users WHERE username = '$username'";
-$result = mysqli_query($conn, $sql);
-$rowCount = mysqli_num_rows($result);
-if ($rowCount>0) {
-    die(json_encode(['error' => 'The username is already registered']));
-}
+    $username = htmlspecialchars($username);
+    $email = htmlspecialchars($email);
+    $passwordHash = password_hash($password, PASSWORD_BCRYPT);
 
-$sql = "INSERT INTO users (username, passwordHash, birthdate, email) VALUES (?, ?, ?, ? )";
-$stmt = $conn->prepare($sql);
-if ($stmt) {
-    $stmt->bind_param("ssss", $username, $hashed, $birthdate, $email);
-    $stmt->execute();
-}else{
-    die("Something went wrong");
-}
+    if (empty($username) OR empty($email) OR empty($password) OR empty($passwordRepeat)) {
+        die(json_encode(['error' => 'All fields are required']));
+    }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        die(json_encode(['error' => 'Email is not valid']));
+    }
+    if (strlen($password)<8) {
+        die(json_encode(['error' => 'Password must be at least 8 characters']));
+    }
+    if (!preg_match('/[A-Z]/', $password)){
+        die(json_encode(['error' => 'Password requires at least one capital letter']));
+    }
+    $specialChars = "!@#$%^&*()_+-=[]{}|;:,.<>?/~`";
+    if (!preg_match('/[' . preg_quote($specialChars, '/') . ']/', $password)){
+        die(json_encode(['error' => 'Password requires at least one symbol']));
+    }
+    if ($password!==$passwordRepeat) {
+        die(json_encode(['error' => 'Passwords do not match']));
+    }
+    // check if email is already registered
+    $result = mysqli_query($conn, "SELECT * FROM users WHERE email = '$email'");
+    $rowCount = mysqli_num_rows($result);
+    if ($rowCount>0) {
+        die(json_encode(['error' => 'The email is already registered']));
+    }
+    // check if username is already registered
+    $result = mysqli_query($conn, "SELECT * FROM users WHERE username = '$username'");
+    $rowCount = mysqli_num_rows($result);
+    if ($rowCount>0) {
+        die(json_encode(['error' => 'The username is already registered']));
+    }
 
+    $sql = "INSERT INTO users (username, passwordHash, birthdate, email) VALUES (?, ?, ?, ? )";
+    $stmt = $conn->prepare($sql);
+    if ($stmt) {
+        $stmt->bind_param("ssss", $username, $hashed, $birthdate, $email);
+        $stmt->execute();
+    }else{
+        die("Something went wrong");
+    }
+}
 $conn->close();
 ?>
+
